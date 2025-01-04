54a01df29be3c9c0b46c27a5b32ff8c17b5ee400
Gary Yaral <gary.yaral@gmail.com>
2025-01-04 10:48:04 -0500

    Last commit 2025

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,8 @@
-$( function(){
-
+$(function () {
   /**************************************************
-  * Area de asignación de eventos y captura de Id's *
-  ***************************************************/
+   * Area de asignación de eventos y captura de Id's *
+   ***************************************************/
 
-  
-  $("#botonHabilitar").click(calcularSacos);
   $("#limpiarTodo").click(resetearTodo);
   $("#btnCalcularMonto").click(calcularMontoTotal);
 
@@ -13,180 +10,126 @@ $( function(){
   var pesoCamion = $("#inputPesoCamion");
   var pesoLibras = $("#inputPesoLibras");
   var inputPrecio = $("#inputPrecio");
-  var btnCalcularSacos = $("#botonHabilitar");
+  var btnCalcularSacos = $("#btnHabilitarCalcular");
   var cajaSacosCalculados = $("#cajaSacosCalculados");
   var cajaMostrarMonto = $("#cajaMostrarMonto");
   var cajaIngresarPrecio = $("#cajaIngresarPrecio");
-
+  var añoActual = $("#añoActual");
+  /**
+   * Configuraciones iniciales
+  */
+  btnCalcularSacos.click(calcularTodo);
+  añoActual.html(new Date().getFullYear())
 
   /******************************
-  * Area de Funciones generales *
-  *******************************/
+   * Area de Funciones generales *
+   *******************************/
 
-  function calcularSacos(){
-
-    var sacosTotales = ((pesoBruto.val()-pesoCamion.val())*2.2)/pesoLibras.val();
-    var sacosTotalesTruncados = Math.trunc(sacosTotales*100)/100;
+  function calcularTodo() {
+    var sacosTotales =
+      ((pesoBruto.val() - pesoCamion.val()) * 2.2) / pesoLibras.val();
+    var sacosTotalesTruncados = Math.trunc(sacosTotales * 100) / 100;
 
     ocultarCajasDeMensajesYPrecio();
 
-
-    if(btnCalcularSacos.html() == "Habilitar"){
-
+    if (btnCalcularSacos.html() == "Habilitar") {
       habilitarTodo();
       let tamañoMaximo = 3;
       let tamañoPrecio = 5;
 
-      document.getElementById("inputPesoLibras").oninput = function(){
-
-	if(this.value.length > tamañoMaximo ) this.value = this.value.slice(0, tamañoMaximo);
-
+      document.getElementById("inputPesoLibras").oninput = function () {
+        if (this.value.length > tamañoMaximo)
+          this.value = this.value.slice(0, tamañoMaximo);
       };
 
-      document.getElementById("inputPrecio").oninput = function(){
-
-	if(this.value.length > tamañoPrecio ) this.value = this.value.slice(0, tamañoPrecio);
-
+      document.getElementById("inputPrecio").oninput = function () {
+        if (this.value.length > tamañoPrecio)
+          this.value = this.value.slice(0, tamañoPrecio);
       };
-
-    }
-
-    else {
-
+    } else {
       quitarClasesValidoInvalido();
 
-      if(pesoBruto.val() === "" && pesoCamion.val() === "" && pesoLibras.val() === "") {
-
+      if (
+        pesoBruto.val() === "" &&
+        pesoCamion.val() === "" &&
+        pesoLibras.val() === ""
+      ) {
         agregarClaseInvalidATodoslosInputs();
         mostrarCajaFaltanCampos();
         pesoBruto.focus();
-
-      }
-
-      else { 
-
-        if(pesoBruto.val() === "") {
-
+      } else {
+        if (pesoBruto.val() === "") {
           pesoBruto.addClass("is-invalid");
           mostrarCajaFaltanCampos();
 
-          if(pesoCamion.val() == "" ){
-
+          if (pesoCamion.val() == "") {
             pesoCamion.addClass("is-invalid");
             pesoLibras.addClass("is-valid");
             mostrarCajaFaltanCampos();
-
-          }
-
-          else {
-
-            pesoCamion.addClass("is-valid"); 
+          } else {
+            pesoCamion.addClass("is-valid");
             pesoBruto.focus();
 
-            if(pesoLibras.val() === ""){
-
+            if (pesoLibras.val() === "") {
               pesoLibras.addClass("is-invalid");
-
-            }
-
-            else {
-
+            } else {
               pesoLibras.addClass("is-valid");
-
-            };
-
-          };
-
-        }
-
-
-        else{
-
+            }
+          }
+        } else {
           pesoBruto.addClass("is-valid");
 
-          if(pesoCamion.val() === "") {
-
+          if (pesoCamion.val() === "") {
             pesoCamion.addClass("is-invalid");
             pesoCamion.focus();
             mostrarCajaFaltanCampos();
 
-            if(pesoLibras.val() === "") {
-
+            if (pesoLibras.val() === "") {
               pesoLibras.addClass("is-invalid");
               pesoCamion.focus();
               mostrarCajaFaltanCampos();
-
-            }
-
-            else {
-
+            } else {
               pesoLibras.classList.addClass("is-valid");
-
-            };
-
-          }
-
-          else{
-
+            }
+          } else {
             pesoCamion.removeClass("is-invalid");
             pesoCamion.addClass("is-valid");
 
-            if(pesoLibras.val() === ""){
-
+            if (pesoLibras.val() === "") {
               pesoLibras.addClass("is-invalid");
               pesoLibras.focus();
               mostrarCajaFaltanCampos();
-
-            }
-
-            else{
-
+            } else {
               pesoLibras.addClass("is-valid");
 
-              if(sacosTotales > 0) {
-
-                if(pesoLibras.val() > 0){
-
+              if (sacosTotales > 0) {
+                if (pesoLibras.val() > 0) {
                   mostrarTotalSacos();
                   calcularMontoTotal();
                   resetearBotonHabilitar();
                   desabilitarInputs();
-
-                }
-
-                else{
-
+                } else {
                   pesoLibras.addClass("is-invalid");
                   cajaSacosCalculados.html("Ingrese valores mayores a '0'");
                   cajaSacosCalculados.removeClass("d-none");
                   cajaSacosCalculados.addClass("alert-danger");
                   pesoLibras.focus();
-
-                };
-
-              }
-
-              else{
-
+                }
+              } else {
                 pesoBruto.focus();
-                pesoBruto.addClass("is-invalid");      
-                cajaSacosCalculados.html("El peso bruto debe ser mayor que el peso del camión.");
+                pesoBruto.addClass("is-invalid");
+                cajaSacosCalculados.html(
+                  "El peso bruto debe ser mayor que el peso del camión."
+                );
                 cajaSacosCalculados.removeClass("d-none");
                 cajaSacosCalculados.addClass("alert-danger");
-
-              };
-
-            };
-
-          };
-
-        };
-
-      };
-
-    };
-
-  };
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
   function resetearTodo() {
     limpiarTodosLosInputs();
@@ -194,80 +137,97 @@ $( function(){
     resetearBotonHabilitar();
     quitarClasesValidoInvalido();
     ocultarCajasDeMensajesYPrecio();
-  };
+  }
 
-  function limpiarTodosLosInputs(){
+  function limpiarTodosLosInputs() {
     pesoBruto.val("");
     pesoCamion.val("");
     pesoLibras.val("");
-  };
+  }
 
-  function ocultarCajasDeMensajesYPrecio(){
-    cajaSacosCalculados.addClass('d-none');
-    cajaMostrarMonto.addClass('d-none');
-    cajaIngresarPrecio.addClass('d-none');
-  };
+  function ocultarCajasDeMensajesYPrecio() {
+    cajaSacosCalculados.addClass("d-none");
+    cajaMostrarMonto.addClass("d-none");
+    cajaIngresarPrecio.addClass("d-none");
+  }
 
-  function calcularMontoTotal(){
-    var sacosTotales = ((pesoBruto.val()-pesoCamion.val())*2.2)/pesoLibras.val();
-    var sacosTotalesTruncados = Math.trunc(sacosTotales*100)/100;
-    var montoTotal=Math.trunc(inputPrecio.val()*sacosTotales*100)/100;
+  function calcularMontoTotal() {
+    let sacos = new Decimal(calcularSacos()).toNumber()
+    let precio = new Decimal(inputPrecio.val());
+    var montoTotal = precio.times(sacos).toFixed(2, 1);
     cajaMostrarMonto.removeClass("d-none");
     cajaIngresarPrecio.removeClass("d-none");
-    cajaMostrarMonto.html("En "+sacosTotalesTruncados+" sacos a "+inputPrecio.val()+" dólares son "+montoTotal+" dólares.");
-  };
+    cajaMostrarMonto.html(
+      "En " +
+        sacos +
+        " sacos a " +
+        precio +
+        " dólares son " +
+        montoTotal +
+        " dólares."
+    );
+  }
 
-  function quitarClasesValidoInvalido(){
-    pesoBruto.removeClass("is-invalid"); 
-    pesoBruto.removeClass("is-valid"); 
-    pesoCamion.removeClass("is-invalid"); 
+  function quitarClasesValidoInvalido() {
+    pesoBruto.removeClass("is-invalid");
+    pesoBruto.removeClass("is-valid");
+    pesoCamion.removeClass("is-invalid");
     pesoCamion.removeClass("is-valid");
-    pesoLibras.removeClass("is-invalid"); 
+    pesoLibras.removeClass("is-invalid");
     pesoLibras.removeClass("is-valid");
-  };
+  }
 
-  function mostrarCajaFaltanCampos(){
+  function mostrarCajaFaltanCampos() {
     cajaSacosCalculados.html("Complete todos los campos.");
     cajaSacosCalculados.removeClass("d-none");
     cajaSacosCalculados.addClass("alert-danger");
-  };
+  }
 
-  function habilitarTodo(){
+  function habilitarTodo() {
     btnCalcularSacos.html("Calcular");
     btnCalcularSacos.removeClass("btn-success");
-    btnCalcularSacos.addClass("btn-primary"); 
-    pesoBruto.attr('disabled',false);
-    pesoCamion.attr('disabled',false);
-    pesoLibras.attr('disabled',false);
+    btnCalcularSacos.addClass("btn-primary");
+    pesoBruto.attr("disabled", false);
+    pesoCamion.attr("disabled", false);
+    pesoLibras.attr("disabled", false);
     inputPrecio.val(0);
     pesoBruto.focus();
-  };
+  }
 
-  function desabilitarInputs(){
-    pesoBruto.attr('disabled',true);
-    pesoCamion.attr('disabled',true);
-    pesoLibras.attr('disabled',true);
-  };
+  function desabilitarInputs() {
+    pesoBruto.attr("disabled", true);
+    pesoCamion.attr("disabled", true);
+    pesoLibras.attr("disabled", true);
+  }
 
-  function agregarClaseInvalidATodoslosInputs(){
+  function agregarClaseInvalidATodoslosInputs() {
     pesoBruto.addClass("is-invalid");
     pesoCamion.addClass("is-invalid");
     pesoLibras.addClass("is-invalid");
   }
 
-  function resetearBotonHabilitar(){
+  function resetearBotonHabilitar() {
     btnCalcularSacos.removeClass("btn-primary");
     btnCalcularSacos.addClass("btn-success");
     btnCalcularSacos.html("Habilitar");
-  };
+  }
 
-  function mostrarTotalSacos(){
-    var sacosTotales = ((pesoBruto.val()-pesoCamion.val())*2.2)/pesoLibras.val();
-    var sacosTotalesTruncados = Math.trunc(sacosTotales*100)/100;
+  function calcularSacos() {
+    let pBruto = new Decimal(pesoBruto.val());
+    let pCamion = new Decimal(pesoCamion.val());
+    let pLibras = new Decimal(pesoLibras.val());
+    let pNeto = pBruto.minus(pCamion).toNumber();
+    let librasTotales = new Decimal(pNeto).times(2.2).toNumber();
+    let sacosTotales = new Decimal(librasTotales).dividedBy(pLibras);
+    return new Decimal(sacosTotales.toFixed(2, 1)).toNumber()
+  }
+
+  function mostrarTotalSacos() {
+    let sacosTotales = calcularSacos()
     cajaSacosCalculados.removeClass("d-none");
     cajaSacosCalculados.removeClass("alert-danger");
-    cajaSacosCalculados.html("Respuesta: Son "+sacosTotalesTruncados+" sacos.");
-  };
-
+    cajaSacosCalculados.html(
+      "Respuesta: Son " + sacosTotales + " sacos."
+    );
+  }
 });
-
